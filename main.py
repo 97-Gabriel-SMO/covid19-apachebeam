db8c4719010dbc0963a10e0e94d1836ea0b9760b
Gabriel Madruga <gabrielsantosmadruga@gmail.com>
2022-09-08 13:20:35 -0300

    Inserido codigo de preprocessamento dos arquivos

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-import apache_beam as beam 
+""" Pipeline de dados ApacheBeam gerador de estatisticas de covid19
 
+Autor: Gabriel Santos Madruga Oliveira
+"""
+from utils import preprocessing_data_csv,preprocessing_lable_csv
+
+
+def main():
+    csv_rows = preprocessing_data_csv('data/HIST_PAINEL_COVIDBR_28set2020.csv',';')
+    label_rows = preprocessing_lable_csv('data/EstadosIBGE.csv',';')
+    print(label_rows)
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,34 @@
+""" Arquivo com funções auxiliares utilizadas no codigo.
+
+Autor: Gabriel Santos Madruga Oliveira
+"""
+
+import csv
+import apache_beam as beam
+
+
+
+def preprocessing_data_csv(filepath:str,delimiter:str):
+    with open(filepath, mode = 'r', encoding='utf-8-sig') as csvfile:
+        collection_result = []
+        reader = csv.DictReader(csvfile,delimiter=delimiter)
+        for row in reader:                
+            collection_result.append(beam.Row(Regiao=row['regiao'],
+                                            UF=row['estado'],
+                                            casosNovos=int(row['casosNovos']),
+                                            obitosNovos=int(row['obitosNovos']),
+                                            Codigo=row['coduf']))
+        return collection_result
+                    
+
+def preprocessing_lable_csv(filepath:str,delimiter:str):
+    with open(filepath, mode = 'r', encoding='utf-8-sig') as csvfile:
+        collection_result = []
+        reader = csv.DictReader(csvfile,delimiter=delimiter)
+        for row in reader:
+            collection_result.append(beam.Row(Governador=row['Governador [2019]'],
+                                            Estado=row['UF [-]'],
+                                            Codigo=row['Código [-]']))
+        return collection_result
+
+
