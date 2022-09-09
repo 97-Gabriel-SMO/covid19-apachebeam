ad377a05fc2b0641563b89d48c0980ea349cf221
Gabriel Madruga <gabrielsantosmadruga@gmail.com>
2022-09-09 00:38:44 -0300

    Inserido log

--- src/main.py
+++ src/main.py
@@ -2,20 +2,23 @@
 
 Autor: Gabriel Santos Madruga Oliveira
 """
-
-import click 
+import logging
+import click
 import apache_beam as beam
 from utils import *
 
 
+LOGGER = logging.getLogger(__name__)
+
+
 @click.command()
-@click.option('--datafile','-d',type=str,prompt='Data file:  ', help='File with covid-19 statistics.')
-@click.option('--labelfile','-l',type = str ,prompt='Label file:  ', help='File with states data.')
-def main(datafile:str,labelfile:str):
-    csv_rows = preprocessing_data(
-        "data/" + datafile, delimiter=";"
-    )
-    label_rows = preprocessing_lable("data/"+labelfile, delimiter=";")
+@click.option("--datafile", "-d", type=str, help="File with covid-19 statistics.")
+@click.option("--labelfile", "-l", type=str, help="File with states data.")
+def main(datafile: str, labelfile: str):
+    LOGGER.info("Starting process...")
+
+    csv_rows = preprocessing_data("data/" + datafile, delimiter=";")
+    label_rows = preprocessing_lable("data/" + labelfile, delimiter=";")
     with beam.Pipeline() as pipeline:
         processed_data = (
             pipeline
@@ -26,17 +29,20 @@ def main(datafile:str,labelfile:str):
             | beam.Map(join_data, label_rows)
             | beam.Filter(remove_missing_values)
         )
+        LOGGER.debug("Generating csv output file...")
         data_export_csv = (
             processed_data
             | "Format CSV string" >> beam.Map(format_output_csv)
             | "Write to CSV output file"
             >> beam.io.WriteToText(
                 "output/test_response",
-                header=('Regiao;Estado;UF;Governador;TotalCasos;TotalObitos'),
+                header=("Regiao;Estado;UF;Governador;TotalCasos;TotalObitos"),
                 file_name_suffix=".csv",
                 shard_name_template="",
             )
         )
+        LOGGER.debug("DONE")
+        LOGGER.debug("Generating json output file...")
         data_export_json = (
             processed_data
             | "Format JSON string" >> beam.Map(format_output_json)
@@ -48,7 +54,11 @@ def main(datafile:str,labelfile:str):
                 shard_name_template="",
             )
         )
+        LOGGER.debug("DONE")
+        LOGGER.debug("Files in output directory.")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="logs/log.cur")
+    LOGGER.setLevel(logging.INFO)
     main()
